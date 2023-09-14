2185f7b8db50b3c23ca4ac4e215aecb9a8fa7173
Dhairya  Shah <dtshah24022006@gmail.com>
2023-09-14 21:49:34 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,69 @@
+from flask import Flask, render_template, request
+import numpy as np
+
+app = Flask(__name__)
+
+
+def row_echelon_form(matrix):
+    m, n = matrix.shape
+    steps = []
+
+    for col in range(n):
+        # Find the pivot row
+        pivot_row = -1
+        for row in range(col, m):
+            if matrix[row, col] != 0:
+                pivot_row = row
+                break
+
+        if pivot_row == -1:
+            continue  # No pivot element in this column, move to the next column
+
+        # Swap the pivot row with the current row (if needed)
+        if pivot_row != col:
+            matrix[[pivot_row, col]] = matrix[[col, pivot_row]]
+            steps.append(f"Swap rows {pivot_row + 1} and {col + 1}:")
+
+        # Make the pivot element 1
+        pivot_element = matrix[col, col]
+        if pivot_element != 1:
+            matrix[col] /= pivot_element
+            steps.append(f"Scale row {col + 1} by {1 / (1 / pivot_element)}:")
+
+        # Eliminate all entries below the pivot
+        for row in range(col + 1, m):
+            factor = matrix[row, col]
+            if factor != 0:
+                matrix[row] -= factor * matrix[col]
+                steps.append(f"Subtract {factor} times row {col + 1} from row {row + 1}:")
+
+    return matrix, steps
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    matrix = np.array([
+        [1, 2, 3],
+        [3, 4, 4],
+        [7, 20, 12]
+    ], dtype=float)
+
+    if request.method == "POST":
+        # Get the matrix as a string from the form input
+        matrix_str = request.form["matrix"]
+
+        try:
+            # Parse the matrix string as a Python list of lists and convert it to a NumPy array
+            matrix = np.array(eval(matrix_str), dtype=float)
+        except Exception as e:
+            error_message = f"Error parsing matrix: {str(e)}"
+            return render_template("index.html", matrix=matrix, error_message=error_message)
+
+    matrix_copy = matrix.copy()
+    matrix_ref, steps = row_echelon_form(matrix_copy)
+
+    return render_template("index.html", matrix=matrix, matrix_ref=matrix_ref, steps=steps)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
